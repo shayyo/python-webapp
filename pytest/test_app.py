aebe704b270a4b6379138e698b1eca124671c27a
shayyo <shay.yosef@gmail.com>
2021-11-29 17:14:53 +0200

    First commit

---
+++ pytest/test_app.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def add(x, y):
+    return x + y
+
+def test_add():
+    assert add(5, 6) == 31
+
+def test_app():
+    r = requests.get("http://shay.freeddns.org:8888")
+    #r = requests.get("http://web:5000")
+    assert r.status_code == 200
+    assert "Hello World I have been seen" in r.text
+
---
+++ s.py
@@ -0,0 +1,4 @@
+import requests
+
+requests.get("192.168.144.3:5000")
+# test
