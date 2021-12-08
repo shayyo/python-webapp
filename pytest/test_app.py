07927259a41d401bf3b82e21cdab167ba50f5b61
shayyo <shay.yosef@gmail.com>
2021-12-08 20:41:44 +0200

    Update test_app.py

--- pytest/test_app.py
+++ pytest/test_app.py
@@ -8,7 +8,6 @@ def test_add():
     assert add(5, 6) == 11
 
 def test_app():
-    # r = requests.get("http://shay.freeddns.org:8888")
     r = requests.get("http://web:5000")
     assert r.status_code == 200
     assert "Hello World I have been seen" in r.text
