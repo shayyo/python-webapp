72752bd72e3877de6eabde08bfdb0f86fbea5c21
shayyo <shay.yosef@gmail.com>
2021-12-27 17:33:40 +0200

    Update test_app.py

--- pytest/test_app.py
+++ pytest/test_app.py
@@ -13,6 +13,9 @@ def test_add2():
 def test_add3():
     assert add(151, 161) == 312
 
+def test_true():
+    asser true == true
+
 def test_app():
     r = requests.get("http://web:5000")
     assert r.status_code == 200
